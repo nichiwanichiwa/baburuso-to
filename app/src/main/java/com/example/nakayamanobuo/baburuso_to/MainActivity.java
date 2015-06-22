8584b70e3c8d4b1e5b1fe4915a5645b3bcf78e38
t-misaki <misaki.tatsuya@nichiwa-system.co.jp>
2015-06-22 09:46:05 +0900

    06/22 手順書作成のために 例で編集したプログラムを 元に戻しました。

--- app/src/main/java/com/example/nakayamanobuo/baburuso_to/MainActivity.java
+++ app/src/main/java/com/example/nakayamanobuo/baburuso_to/MainActivity.java
@@ -59,11 +59,7 @@ public class MainActivity extends Activity {
                         break;
                     case 4:
                         TextView e = (TextView) findViewById(R.id.textView7);
-                        e.setText("私は修正部分です。\n");
-                        break;
-                    case 5:
-                        TextView f = (TextView) findViewById(R.id.textView7);
-                        f.setText("私は追加部分です。\n");
+                        e.setText("お名前は" + name[x] + "点数は" + score[y] + "です。\n");
                         break;
                     default:
                         break;
